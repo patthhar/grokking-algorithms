03ad94565879a9069d9850dbaa6201b128442d0d
patthhar <parthdroidworld@gmail.com>
2023-07-04 14:37:40 +0530

    Binary search added

---
+++ main.py
@@ -0,0 +1,6 @@
+from binary_search import binary_search
+
+if __name__ == '__main__':
+    my_list = [1, 3, 5, 7, 10, 14, 23, 43]
+    print(binary_search.binary_search(my_list, 4))
+    print(binary_search.binary_search(my_list, 14))
